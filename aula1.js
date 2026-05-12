56b6b35c3c43f203c8c86d36b85f4df8afc43ecc
LucaslvesdaSilva <alves.silva.lucas2212@escola.pr.gov.br>
2026-05-12 18:15:43 -0300

    Create aula1.js

---
+++ aula1.js
@@ -0,0 +1,15 @@
+// let nome = "lucas";
+// let idade = 16;
+// let cidade = "cascacity";
+
+// console.log(`aluno: ${nome}, idade: ${idade}, cidade: ${cidade}`);
+
+// const multiplicacao = (x, y) => x * y;
+// const soma = (x, y) => x + y;
+// const subtracao = (x, y) => x - y;
+// const divisao = (x, y) => x / y;
+
+// console.log(`Soma : ${ soma (10 , 5) } `) ;
+// console.log(`multiplicacao : ${ multiplicacao (10 , 5) } `) ;
+// console.log(`subtração : ${ subtracao (10 , 5) } `) ;
+// console.log(`divisao : ${ divisao (10 , 5) } `) ;
