ff1ae32bc9bd574a7fe64cfab4ab1a227cf448e8
LucaslvesdaSilva <alves.silva.lucas2212@escola.pr.gov.br>
2026-05-26 18:13:29 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,15 +1,6 @@
-// let nome = "lucas";
-// let idade = 16;
-// let cidade = "cascacity";
 
-// console.log(`aluno: ${nome}, idade: ${idade}, cidade: ${cidade}`);
+let nome = "lucas";
+let idade = 16;
+let cidade = "cascacity";
 
-// const multiplicacao = (x, y) => x * y;
-// const soma = (x, y) => x + y;
-// const subtracao = (x, y) => x - y;
-// const divisao = (x, y) => x / y;
-
-// console.log(`Soma : ${ soma (10 , 5) } `) ;
-// console.log(`multiplicacao : ${ multiplicacao (10 , 5) } `) ;
-// console.log(`subtração : ${ subtracao (10 , 5) } `) ;
-// console.log(`divisao : ${ divisao (10 , 5) } `) ;
+console.log(`ola! Meu nome é ${nome}, tenho: ${idade} anos, e sou de: ${cidade}`);
